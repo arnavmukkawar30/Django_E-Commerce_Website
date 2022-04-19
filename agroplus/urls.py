7d309d546411187a43b48bcd8d7979cee329a8be
arnavmukkawar30 <arnavmukkawar@gmail.com>
2022-04-19 22:37:22 +0530

    second commit

--- agroplus/urls.py
+++ agroplus/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 from agroplus import views
 urlpatterns = [
     path('', views.home, name='home'),
+    path('home1', views.home1, name='home1'),
 ]
--- agroplus/views.py
+++ agroplus/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def home(request):
+    return render(request, 'home1.html')
+
+def home1(request):
     return render(request, 'home.html')
